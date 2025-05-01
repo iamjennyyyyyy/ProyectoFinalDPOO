5d16e6c3051a34cd41b2050d444d3f34379d3d05
iamjennyyyyyy <jenn06rh@gmail.com>
2025-05-01 17:30:17 -0400

    new commit2

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -10,11 +11,35 @@ public class Biblioteca {
 	private String horario;
 	private String nombreCompletoAdmin;
 	private int annosEnCargo;
-	private ArrayList<UsuarioAcreditado> usuarios = new ArrayList<UsuarioAcreditado>();
-	private ArrayList<Libro> libros = new ArrayList<Libro>();
-	private ArrayList<Revista> revistas = new ArrayList<Revista>();
-	private ArrayList<Articulo> articulos = new ArrayList<Articulo>();
-	
+	private ArrayList<Trabajador> trabajadores;
+	private ArrayList<UsuarioAcreditado> usuarios;
+	private ArrayList<Libro> libros;
+	private ArrayList<Revista> revistas;
+	private ArrayList<Articulo> articulos;
+	private ArrayList<Prestamo> prestamosTotales;
+
+	public Biblioteca(int id, String nombre, String provincia,
+			String municipio, String horario, String nombreCompletoAdmin,
+			int annosEnCargo, ArrayList<Trabajador> trabajadores,
+			ArrayList<UsuarioAcreditado> usuarios, ArrayList<Libro> libros,
+			ArrayList<Revista> revistas, ArrayList<Articulo> articulos,
+			ArrayList<Prestamo> prestamosTotales) {
+
+		this.id = id;
+		this.nombre = nombre;
+		this.provincia = provincia;
+		this.municipio = municipio;
+		this.horario = horario;
+		this.nombreCompletoAdmin = nombreCompletoAdmin;
+		this.annosEnCargo = annosEnCargo;
+		trabajadores = new ArrayList<Trabajador>();
+		usuarios = new ArrayList<UsuarioAcreditado>();
+		libros = new ArrayList<Libro>();
+		revistas = new ArrayList<Revista>();
+		articulos = new ArrayList<Articulo>();
+		prestamosTotales = new ArrayList<Prestamo>();
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -78,34 +103,63 @@ public class Biblioteca {
 	}
 
 	//Metodo para realizar o no el prestamo
-	public boolean solicitarPrestamo(int id, Publicacion pub){
+	public boolean solicitarPrestamo(int id, Publicacion pub, Trabajador trabajador){
 
 		UsuarioAcreditado user = buscarUsuarioPorId(id);
 
 		boolean esUsuario = user != null? true : false;
-
+		boolean dosSemanasTranscurridas = true;
 		boolean prestamoRealizado = false;
 
 		if(esUsuario){
-			if(pub.getCantEjemplares() > 2){
-				if(user.getPrestamos().size() < 3){
-					pub.disminuirStock();
-					LocalDateTime fechaActual = LocalDateTime.now();
-					int tiempo = pub.tiempoMaximoPrestamo();
-					LocalDateTime fechaDevolucion = fechaActual.plusDays(tiempo);
-					user.setPrestamos(new Prestamo(fechaActual,fechaDevolucion,pub,user,nombreCompletoAdmin));
-					prestamoRealizado = true;
+
+			for(Prestamo p : user.getPrestamos()){
+				if(p.getPub().equals(pub)){
+					if(p.getFechaDevolucion() != null && !p.getFechaDevolucion().plusDays(14).isAfter(LocalDate.now()))
+						dosSemanasTranscurridas = false;
+				}
+			}
+			if(dosSemanasTranscurridas){
+				if(pub.getCantEjemplares() > 2){
+					if(user.getPrestamos().size() < 3){
+						pub.disminuirStock();
+						LocalDateTime fechaActual = LocalDateTime.now();
+						int tiempo = pub.tiempoMaximoPrestamo();
+						LocalDateTime fechaDevolucion = fechaActual.plusDays(tiempo);
+						Prestamo p = new Prestamo(fechaActual,fechaDevolucion,pub,user,trabajador);
+						user.getPrestamos().add(p);
+						prestamosTotales.add(p);
+						prestamoRealizado = true;
+					}
+					else
+						throw new IllegalArgumentException("El usuario tiene ya 3 prestamos.");
 				}
 				else
-					throw new IllegalArgumentException("El usuario tiene ya 3 prestamos.");
+					throw new IllegalArgumentException("Publicacion no disponible.");
 			}
 			else
-				throw new IllegalArgumentException("Publicacion no disponible.");
+				throw new IllegalArgumentException("No han transcurrido dos semanas de su devolucion de esta publicacion.");
 		}
 		else
 			throw new IllegalArgumentException("Usuario no encontrado.");
 
 		return prestamoRealizado;
+	}
+
+	public void devolverPublicacion(int id, Publicacion pub){
+
+		UsuarioAcreditado user = buscarUsuarioPorId(id);
+
+		if(user != null){
+			for(Prestamo p : user.getPrestamos()){
+				if(p.getPub().equals(pub)){
+					LocalDate fechaActual = LocalDate.now();
+					p.setFechaDevolucion(fechaActual);
+					prestamosTotales.add(p);
+					user.getPrestamos().remove(p);
+				}
+			}
+		}
 	}
 
 	public UsuarioAcreditado buscarUsuarioPorId(int id){
